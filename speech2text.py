b1c8e083a1f1588df620f77e0d792a07a59cca6d
Eura Shin <eura.shin@gmail.com>
2019-03-11 14:56:45 -0400

    changed speech2text

--- speech2text.py
+++ speech2text.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import speech_recognition as sr
+import time
 import json 
 
 #IMPORTANT: all methods must return jsonp
@@ -6,10 +8,19 @@ import json
 app = Flask(__name__)
 
 #simple speech to text method goes here
-@app.route('/speechToText', methods=['GET'])
-def speechToText():
+@app.route('/speechToString', methods=['GET'])
+def speechToString():
     funcname = request.args.get('callback')
-    return jsonp(dict(success="It worked"), funcname)
+    string_in = ''
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+    try:
+        string_in = r.recognize_google(audio)
+    except:
+        pass
+
+    return jsonp(dict(success=string_in), funcname)
+
 
 #converts your return value into jsonp type
 # needs: a dictionary of return values, and a funcname
