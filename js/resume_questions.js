56db9cfb618e6d0e152494ceae7cccdfd95df415
eurashin <eura.shin@gmail.com>
2019-04-09 11:17:30 -0400

    changed question format

--- js/resume_questions.js
+++ js/resume_questions.js
@@ -1,19 +1,27 @@
-var resume_1_question_array=[
+var base_question_array = [
+    /*header questions*/
     "What is your name?", 
     "What is your job title?", 
     "What is your phone number?", 
     "What is your LinkedIn",
     "What is your email?", 
     "What is your summarized career objective?", 
+    /*first experience*/
     "List one job you've had in the past.", 
     "When did you begin this position? (month, year)", 
     "When did you end this position? (month, year)",
-                        "What is the name of the company that you worked for?", "What city is this company located? (City, State Abbrv.)",
-                        "Add a job description.",
-                        "List another job you've had in the past.", "When did you hold this position? (month year - month year)",
-                        "What is the name of the company that you worked for?", "What city is this company located? (City, State Abbrv.)",
-                        "Add a job description.",
-                        "List another job you've had in the past.", "When did you hold this position? (month year - month year)",
-                        "What is the name of the company that you worked for?", "What city is this company located? (City, State Abbrv.)",
-                        "Add a job description."];
-                        
+    "What is the name of the company that you worked for?", 
+    "Where is this company located? (City, State.)",
+    "Describe your responsibilities during this position."
+    /*second experience*/
+    "List another job you've had in the past.", 
+    "When did you begin this position? (month, year)", 
+    "When did you end this position? (month, year)",
+    "What is the name of the company that you worked for?", 
+    "Where is this company located? (City, State.)",
+    "Describe your responsibilities during this position."
+]
+
+var cs_resume_questions = [
+
+]
