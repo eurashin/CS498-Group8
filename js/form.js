28598ab9c33aac935ea76de538a5004baab6ac87
Evan Yang <evan.yang@uky.edu>
2019-04-11 19:36:48 -0400

    commented out for loop that was causing error in exportPDF()

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-var answer_array; 
+var answer_array;
 
 function setAnswers() {
     answer_array =  JSON.parse(window.localStorage.getItem("answer_array"));
@@ -9,7 +9,7 @@ function setAnswers() {
             $('#' + String(i)).css('color', 'grey'); //should have prompt
             $('#' + String(i)).css('border', '1px solid black'); //should have border
             $('#' + String(i)).css('min-width', '20px');
-           
+
         }
         else {
             $("#" + String(i)).text(answer_array[i]); //grab the element
@@ -21,8 +21,8 @@ function setAnswers() {
         $(this).css('font-style', 'normal');
         $(this).text('');
         answer_array[this.id] = 'filled';
-    }) 
-            
+    })
+
 }
 
 
@@ -32,13 +32,13 @@ function setAnswers() {
 // https://github.com/MrRio/jsPDF/issues/476
 function exportPDF()
 {
-    $('.form-element').css('border-style', 'none');
-    
+    /*$('.form-element').css('border-style', 'none');
+
     for(var i=0; i<answer_array.length; i++) {
         if(!answer_array[i]) { //is empty
-            $('#' + String(i)).text(''); 
+            $('#' + String(i)).text('');
         }
-    }
+    }*/
 
 
     html2canvas(document.querySelector("#resume")).then(canvas => {
